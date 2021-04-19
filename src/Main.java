c84beffbb7973a14f429d4da492d5775c23f3968
avbochkareva <81125737+avbochkareva@users.noreply.github.com>
2021-04-19 20:24:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main1 {
 
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         System.out.print("Введите стоимость билета: ");
-        long num = in.nextInt();
-            if (num >= 20)
-            {
-                num = num / 20;
-                System.out.printf("Вам начислено: " + num + " мили(-ей)");
-            }
-            else
-                System.out.println("Вам начислено 0 милей");
+        long price_ticket = in.nextInt();
+        long bonus = 0;
+        bonus = price_ticket / 20;
+        System.out.printf("Вам начислено: " + bonus + " мили(-ей)");
     }
 }
